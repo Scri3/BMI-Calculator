d250cb7048a71fc56d27521f6176688d65c09e56
Danial Goodarzi <117368981+Scri3@users.noreply.github.com>
2024-03-23 20:02:16 +0330

    Add files via upload

---
+++ BMI Calculator/Body BMI/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Body_BMI
+{
+    internal class Person
+    {
+        public string Name;
+        public string Family;
+        public int Height;
+        public int Weight;
+
+        public static float BMI_num(float a, float c)
+        {
+            float b = c / 100;
+            return a / (b * b);
+
+        }
+        public static bool BMI(float a, float c)
+        {
+            float b = c / 100;
+
+            bool result;
+            if ((a / (b * b) >= 18.5) && (a / (b * b) <= 24.9)) 
+            {
+                result = true;
+
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+        public static string BMI_Status(float a, float c)
+        {
+            float b = c / 100;
+
+            string Status;
+
+            if ((a / (b * b) >= 18.5) && (a / (b * b) <= 24.9))
+
+            {
+                Status = "You are Healthy";
+            }
+            else
+            {
+                Status = "You are not Healthy";
+            }
+
+
+
+            //ostad inja mikhastam age az 24.9 balatar shod bege ezafe vazn dari age kamtar az 18.5 shod bege kam vazni, vali chon 2,3 ta if tarif kardm moghe return
+            //kardan error midad. vase hamin bikhialesh shodam va ba if va else barname ro neveshtam.
+            //if (a / (b * b) > 24.9) 
+            //{
+            //    Status = "You are OverWeight";
+            //}
+            //if (a / (b * b) < 18.5)
+
+            //{
+            //    Status = "You have WeightLoss";
+
+            //}
+            return Status;
+        }
+    }
+}
+
+
+
+
+
+
+    
+    
---
+++ BMI Calculator/Body BMI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Body_BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person P1=new Person();
+            Console.WriteLine("Hello");
+            Console.WriteLine();
+            Console.WriteLine("Enter your First Name: ");
+            P1.Name =Console.ReadLine();
+            Console.WriteLine("Enter your Last Name: ");
+            P1.Family=Console.ReadLine();
+            Console.WriteLine("Enter your Weight(KG): ");
+            P1.Weight=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Height(CM): ");
+            P1.Height=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("We counted your BMI number. ");
+            Console.WriteLine();
+            Console.WriteLine("Your BMI number is: " + Person.BMI_num(P1.Weight, P1.Height));
+            Console.WriteLine();
+
+            Console.WriteLine("Your Health Status is "+Person.BMI(P1.Weight,P1.Height));
+
+            Console.WriteLine("Need more infomation? Then Press Enter to continue. if not, Close the program.");
+            Console.ReadLine();
+            Console.WriteLine(Person.BMI_Status(P1.Weight, P1.Height));
+            Console.ReadLine();
+            //to in barname age BMI shoma beine 18.5 va 24.9 bashe vaziate salamate badane shoma saleme dar gheire in sorat agar balaye 24.9 bashe ezafe vazn va zire 18.5 
+            //kaheshe vazn darid.
+
+        }
+    }
+}
